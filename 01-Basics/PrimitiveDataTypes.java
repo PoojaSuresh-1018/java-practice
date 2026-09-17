ea87e5b9dea2e4b5a620c56229064f046985a999
PoojaSuresh-1018 <Poojasuresh1018@gmail.com>
2026-09-17 20:29:22 +0530

    Primitive Data Types

---
+++ 01-Basics/PrimitiveDataTypes.java
@@ -0,0 +1,22 @@
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+        
+        byte theByte = -111;
+        short theShort = 28899;
+        int age = 19;
+        long height = 1000000L;
+        float pi = 3.14F;
+        double doublePie = 3.1456;
+        boolean foodie = true;
+        char favouriteLetter = 'P';
+
+        System.out.println("Byte: " + theByte);
+        System.out.println("Short: " + theShort);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Pi: " + pi);
+        System.out.println("Double Pie: " + doublePie);
+        System.out.println("Foodie: " + foodie);
+        System.out.println("Favourite Letter: " + favouriteLetter);
+}
+}
