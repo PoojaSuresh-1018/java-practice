3501bce6ec85e84b230db37149f1bb8c926b48e0
PoojaSuresh-1018 <Poojasuresh1018@gmail.com>
2026-09-21 21:19:34 +0530

    Feat: Added Operators

---
+++ 01-Basics/Operators.java
@@ -0,0 +1,35 @@
+public class Operators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+      
+        System.out.println("Arithmetic Operators:");
+        System.out.println(a + b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+        System.out.println("Comparison Operators:");
+        System.out.println(a<=b);
+        System.out.println(a>b);
+        System.out.println(a!=b);
+
+        System.out.println("Logical Operators:");
+        System.out.println(a<b && a!=b);
+        System.out.println(a>b && a!=b);
+        System.out.println(a>=b || a>=5);
+
+        System.out.println("Unary Operators:");
+        System.out.println(a++);
+        System.out.println("a = " + a);
+        System.out.println("b = " + --b);
+        
+        System.out.println("Assignment Operators:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("a + 10 = " + (a+=10));
+        System.out.println("b * 2 = " + (b*=2));
+        System.out.println("b = "+ b +"\nb - 2 = " + (b-=2));
+        
+    }
+    
+}
